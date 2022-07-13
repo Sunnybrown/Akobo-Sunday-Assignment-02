93f71f823b12c2549caab44f3d94354b2872cb5c
Sunday Akobo <asundaybrown@gmail.com>
2022-07-13 15:06:01 +0000

    I added an array of numbers to line 20 and summed them up in line 23 for assignment 1.
    For assignment 2, i added an array of number in line 45 and did a count of even numbers in line 51 using the for.each function.

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,22 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+function sumOfNumbers() {
+
+  // Declaring the variable for the array of numbers
+  const Nums = [27, 10, 75, -30, 20, 190, 300, -224, 60, 174,3, 34, 33, 44, 57, 56, 18, 22, 1, 0, 2, 4, 66, -54, 23, 67, -87, 1, 0, -3, 2, 6, -50, -4, 7, 32, -12, 13, 14, -41, 15, -52, -44, -66, 80, 10, 20, 90, -54, 67, -31, 21, -21, -23, 23, 34, 43, 55, 67, 89, -12, 32, 43, -35, -32, 31, 96, 97, -70, -76, 98, -90, 0, 7, 9]
+
+  // Suming the numbers in the array
+  const result = Nums.reduce(function (x, y) {
+    return x + y;
+  }, 0);
+
+    console.log(result)
+    return Nums.length
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+sumOfNumbers()
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +39,29 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function countEvenNumbers() {
+
+  // Declaring the variable for the array of numbers
+  const arr = [17, 15, 20, 2, 3, 45, 50, 77, 7]
+
+  // variable  for the even number
+  var evenNumbers = [];
+
+  // logging the even numbers with for.each functions
+  arr.forEach(number => {
+    if (number % 2 === 0) {
+      evenNumbers.push(number);
+    }
+  });
+
+  // login the array of even numbers
+  console.log(evenNumbers)
+  // console.log(evenNumbers.length)
+  return arr.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+countEvenNumbers()
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
